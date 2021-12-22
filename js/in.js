136fbef9715dc76d39fb696eaaff3e6b2f9cee15
heloMayo <israaahusu@gmail.com>
2021-12-22 09:19:09 +0200

    final files

--- js/in.js
+++ js/in.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
-    $("p").lettering();
+    $("#welcome").lettering();
     $(window).on("load",function(){
         window.setTimeout(()=>{
-            // $("p").css({"transform":"skewY(-150deg)translateY(-500px)","transition":"1s ease-in-out"})
-        //    setTimeout(()=>{ window.location.href="./index.html"},800)
+            $("img").css({"transform":"skewX(90deg)translate(80px,-400px)","transition":"1s ease-in-out"})
+            $("#welcome").css({"transform":"skewY(-150deg)translateY(-500px)","transition":"1s ease-in-out"})
+           setTimeout(()=>{ 
+               window.location.href="./in.html"
+            },800)
     
-        },3000)
+        },2000)
 
     })
 
--- js/main.js
+++ js/main.js
@@ -1,80 +1,80 @@
 // alert("kcksklckl")
-$(document).ready(function(){
-   
-        console.log("jellli")
-        // $(".briefEx").toggleClass("myFade");
-   
-       
-   $(window).on("load",function(){
+$(document).ready(function () {
+  console.log("jellli");
+  // $(".briefEx").toggleClass("myFade");
 
-    $("#logo").fadeToggle(1000 , ()=>{
-    $('#myIntro').show(1000,function(){
-    $(this).css("opacity","1")
-    window.setTimeout(()=>{
+  $(window).on("load", function () {
+    $("#logo").fadeToggle(1000, () => {
+      $("#myIntro").show(1000, function () {
+        $(this).css("opacity", "1");
+        window.setTimeout(() => {}, 1000);
+        $(".personCon").slideDown(1000, () => {
+          $(".leftNav li").each(function () {
+            console.log("kdjkjd");
+            $(this).slideDown(1000);
+          });
+          window.setTimeout(() => {
+            $(".personCon img").fadeToggle(800, () => {
+              lo();
+              (() => {
+                $("#job").css("opacity", "1");
+              })();
+            });
+          }, 1000);
+          //   window.setTimeout(()=>{
+          //     $("#job").css("opacity","1")
 
-        
-    },1000)
-    $(".personCon").slideDown(1000,()=>{
-    $(".leftNav li").each(
-        function(){
-            console.log("kdjkjd")
-            $(this).slideDown(1000)
-    })
-    window.setTimeout(()=>{
-        $(".personCon img").fadeToggle(800,()=>{lo()})
+          // },1000)
+        });
+      });
+    });
+  });
+  console.log("Hello from Jquery file");
+  $(".Ritems li").on("click", function () {
+    console.log($(this).index());
+    let ind = $(this).index();
+    $(".RitemsEx div").each(function () {
+      //   console.log("the other "+$(this).index())
+      if ($(this).index() == ind) {
+        // console.log($(this))
+        $(this).css("z-index", "1000");
+      } else {
+        $(this).css("z-index", "1");
+      }
+    });
+  });
+  $("#eduBtn").on("click", function () {
+    $(".RitemsEx div").eq(0).css("z-index", "2000");
+    $(".Ritems li").eq(0).addClass("showWhichSection");
+  });
 
-    },1000)
-    
-
-    })
-
-  })
-}) })
-console.log("Hello from Jquery file")
-  $(".Ritems li").on("click",function(){
-      console.log($(this).index())
-      let ind=$(this).index()
-      $(".RitemsEx div").each(function(){
-         
-        //   console.log("the other "+$(this).index())
-       if($(this).index()==ind)
-       {
-        console.log($(this))
-        $(this).css("z-index","1000")
-       }
-       else{
-        $(this).css("z-index","1")
-       }
-      })
-
-      
-  })
-
-  $(".RitemsEx > div").on("mouseenter",function(){
+  $(".RitemsEx > div").on("mouseenter", function () {
     //   console.log("hovered section ", $(".Ritems li").eq($(this).index()).text())
-    
-    $(".Ritems li").eq($(this).index()).addClass("showWhichSection")
-})
-$(".RitemsEx div").on("mouseleave",function(){
+
+    $(".Ritems li").eq($(this).index()).addClass("showWhichSection");
+  });
+  $(".RitemsEx div").on("mouseleave", function () {
     // console.log("hovered section ", $(".Ritems li").eq($(this).index()).text())
-  $(".Ritems li").eq($(this).index()).removeClass("showWhichSection")
-})
+    console.log("hittt");
+    $(".Ritems li").eq($(this).index()).removeClass("showWhichSection");
+  });
 
-
-$(".mailShowBtn").one("mouseenter",function(){
-    $(this).fadeToggle(1000,()=>{
-    $(".mailBox").css("transition"," all,2s ease").removeClass("invisible").addClass("visible")
-    })
-})
+  $(".mailShowBtn").one("mouseenter", function () {
+    $(this).fadeToggle(1000, () => {
+      $(".mailBox")
+        .css("transition", " all,2s ease")
+        .removeClass("invisible")
+        .addClass("visible");
+    });
+  });
 });
 
-function lo (){
-    $(".myName").children().each(
-        function(){
-           
-               $(this).show(2000)
-           
-            console.log("child"+ $(this).innerText)
-          
-        })
+function lo() {
+  $(".myName")
+    .children()
+    .each(function () {
+      $(this).show(2000);
+
+      // console.log("child"+ $(this).innerText)
+    });
 }
