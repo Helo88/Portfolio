fa7a5c81772be4b2c9ac326fe8f9cd9351def99e
heloMayo <israaahusu@gmail.com>
2021-12-22 09:30:31 +0200

    final files

--- js/in.js
+++ js/in.js
@@ -3,12 +3,12 @@ $(document).ready(function () {
     $(window).on("load",function(){
         window.setTimeout(()=>{
             $("img").css({"transform":"skewX(90deg)translate(80px,-400px)","transition":"1s ease-in-out"})
-            $("#welcome").css({"transform":"skewY(-150deg)translateY(-500px)","transition":"1s ease-in-out"})
+            $("#welcome").css({"transform":"skewY(-150deg)translateY(-3000px)","transition":"1s ease-in-out"})
            setTimeout(()=>{ 
-               window.location="./in.html"
+               window.location.href="./in.html"
             },800)
     
-        },2000)
+        },1500)
 
     })
 
